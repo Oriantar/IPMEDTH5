b422e4858531cd35ffed3f66720221eb88ef41ec
Tim Maurizio Dullaart <77988256+Oriantar@users.noreply.github.com>
2024-04-15 14:17:01 +0200

    Update InbraakMeldingController.php

--- LaravelOmgeving/app/Http/Controllers/InbraakMeldingController.php
+++ LaravelOmgeving/app/Http/Controllers/InbraakMeldingController.php
@@ -13,18 +13,22 @@ class InbraakMeldingController extends Controller
     public function index(Request $request)
     {
         $sensorId = $request->segment(2); 
-    
+        $camera = $request->url;
+        
 
         $sensor = sensorids::where('sensorid', $sensorId)->first();
     
-
+        
         if ($sensor) {
             $inbraakMelding = new inbraakMelding;
             $inbraakMelding->sensor_id = $sensorId;
             $inbraakMelding->user_id = $sensor->user_id;
             $inbraakMelding->save();
         }
-    
+        if($camera == $inbraakMelding->sensor_id->camerabeeld)continue;
+        else{
+            $inbraakMelding->sensor_id->camerabeeld = $camera
+        }
         return redirect('/');
     }
     
