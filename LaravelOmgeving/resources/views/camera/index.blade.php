44bece9d56adcafec4a9b1eb400dcb872df60fbe
Oriantar <tim.maurizio@gmail.com>
2024-04-15 16:08:45 +0200

    added test embed instead of iframe

--- LaravelOmgeving/resources/views/camera/index.blade.php
+++ LaravelOmgeving/resources/views/camera/index.blade.php
@@ -6,5 +6,6 @@
     <iframe id="ytplayer" type="text/html" width="640" height="360"
   src={{"$sensor->cameraBeeld"}}
   frameborder="0"></iframe>
+  <embed type="text/html" src={{"$sensor->cameraBeeld"}}  width="500" height="200">
 @endforeach
 </x-app-layout>
