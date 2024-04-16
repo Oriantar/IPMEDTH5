474617e4a275e5ad480167cc7ef5b5ee17ecae5c
FloriBanaan <FloriBanaanLOL@gmail.com>
2024-04-16 13:10:12 +0200

    layout mooier gemaakt

--- LaravelOmgeving/resources/views/Instellingen/index.blade.php
+++ LaravelOmgeving/resources/views/Instellingen/index.blade.php
@@ -1,31 +1,33 @@
 <x-app-layout>
     <div class="achtergrond">
-        <div class="instellingen-container">
-            <p> Alarm </p>
+        <div class="instellingen-pagina-container">
+            <div class="instellingen-container">
+                <p> Alarm </p>
 
-            <!-- <button id="myButton" class="button-white">Click me</button> -->
-            
-            <label id="alarmButton" data-alarm="<?php echo htmlspecialchars($alarm->alarm); ?>" class="button-white">
-                <input id="alarmButtonCheckbox" type="checkbox">
-                <a href="/alarm">
-                <div id="alarmButtonBackground" class="toggle-switch-background">
-                    <div id="alarmHandle" class="toggle-switch-handle"></div>
+                <!-- <button id="myButton" class="button-white">Click me</button> -->
+                
+                <label id="alarmButton" data-alarm="<?php echo htmlspecialchars($alarm->alarm); ?>" class="button-white">
+                    <input id="alarmButtonCheckbox" type="checkbox">
+                    <a href="/alarm">
+                    <div id="alarmButtonBackground" class="toggle-switch-background">
+                        <div id="alarmHandle" class="toggle-switch-handle"></div>
+                    </div>
+                    </a>
+                </label>
+                <p> Notificaties </p>
+                <label id="notificatieButton" data-notificatie="<?php echo htmlspecialchars($notification->notification); ?>" class="button-white">
+                    <input id="notificatieButtonCheckbox" type="checkbox">
+                    <a href="/notification">
+                    <div id="notificatieButtonBackground" class="toggle-switch-background">
+                        <div id="notificatieHandle" class="toggle-switch-handle"></div>
+                    </div>
+                    </a>
+                </label>
+                <div class="alarmtest-container">
+                    <hr>
+                    <button> Test alarm </button>
+                    <p> Zodra deze knop wordt ingedrukt, zal het alarm 5 seconden lang afgaan. </p>
                 </div>
-                </a>
-            </label>
-            <p> Notificaties </p>
-            <label id="notificatieButton" data-notificatie="<?php echo htmlspecialchars($notification->notification); ?>" class="button-white">
-                <input id="notificatieButtonCheckbox" type="checkbox">
-                <a href="/notification">
-                <div id="notificatieButtonBackground" class="toggle-switch-background">
-                    <div id="notificatieHandle" class="toggle-switch-handle"></div>
-                </div>
-                </a>
-            </label>
-            <div class="alarmtest-container">
-                <hr>
-                <button> Test alarm </button>
-                <p> Zodra deze knop wordt ingedrukt, zal het alarm 5 seconden lang afgaan. </p>
             </div>
         </div>
 
