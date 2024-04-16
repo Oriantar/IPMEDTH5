d317cc41b52499d5c1bf6621d68f5b58046896ac
Oriantar <tim.maurizio@gmail.com>
2024-04-17 01:03:52 +0200

    added inbraakmelding testcase

---
+++ LaravelOmgeving/tests/Feature/Inbraakmelding/InbraakmeldingTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\InbraakMelding;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class InbraakMeldingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function flappycam_can_make_inbraak_melding()
+    {
+        $count = InbraakMelding::count();
+        $user = User::factory()->create();
+        DB::table('sensorids')->insert([
+            'id' => 1,
+            'sensorid' => 'flappy_is_beste',
+            'cameraNaam' => 'flappy_is_beste',
+            'user_id' => $user->id,            
+        ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->patch('/inbraakmelding/flappy_is_beste/camerabeeld?url=1', [
+                'name' => 'Test User',
+                'email' => $user->email,
+            ]);
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/');
+        $count += 1;
+        $this->assertSame($count, InbraakMelding::count());
+    }
+}
