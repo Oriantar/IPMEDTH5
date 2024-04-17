433d4c601804909718a475d14273288b64936a82
FloriBanaan <FloriBanaanLOL@gmail.com>
2024-04-17 08:24:23 +0200

    flappycam logo

--- LaravelOmgeving/resources/views/layouts/navigation.blade.php
+++ LaravelOmgeving/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('camera.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('img/flappycam.png') }}" style="width: 60px; height: auto;">
                     </a>
                 </div>
 
@@ -15,7 +15,7 @@
                     <!-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link> -->
-                    <x-nav-link :href="route('camera.index')" :active="request()->routeIs('camera.index')">
+                    <!-- <x-nav-link :href="route('camera.index')" :active="request()->routeIs('camera.index')">
                         {{__('Camera')}}
                     </x-nav-link>  
                     <x-nav-link :href="route('meldingen.index')" :active="request()->routeIs('meldingen.index')">
@@ -23,7 +23,7 @@
                     </x-nav-link>
                     <x-nav-link :href="route('instellingen.index')" :active="request()->routeIs('instellingen.index')">
                         {{__('Instellingen')}}
-                    </x-nav-link>
+                    </x-nav-link> -->
                 </div>
             </div>
 
@@ -31,14 +31,14 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                        <button class="naam-knop" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <p>{{ Auth::user()->name }}</p>
 
-                            <div class="ms-1">
+                            <!-- <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
-                            </div>
+                            </div> -->
                         </button>
                     </x-slot>
 
@@ -116,12 +116,6 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-center h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('camera.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
@@ -129,49 +123,15 @@
                         {{ __('Dashboard') }}
                     </x-nav-link> -->
                     <x-nav-link :href="route('camera.index')" :active="request()->routeIs('camera.index')">
-                        {{__('Camera')}}
+                        <p> {{__('Camera')}} </p>
                     </x-nav-link>  
                     <x-nav-link :href="route('meldingen.index')" :active="request()->routeIs('meldingen.index')">
-                        {{__('Meldingen')}}
+                        <p> {{__('Meldingen')}} </p>
                     </x-nav-link>
                     <x-nav-link :href="route('instellingen.index')" :active="request()->routeIs('instellingen.index')">
-                        {{__('Instellingen')}}
+                        <p> {{__('Instellingen')}} </p>
                     </x-nav-link>
                 </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
             </div>
 
             <!-- Hamburger -->
